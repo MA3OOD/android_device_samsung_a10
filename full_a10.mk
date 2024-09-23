a3f215917538a95c309ec4c5438fb8156d501f2a
Muhammad Masood <163732055+Masood-J@users.noreply.github.com>
2024-09-24 00:17:53 +0500

    Update full_a10.mk

--- full_a10.mk
+++ full_a10.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a10/device.mk)
+$(call inherit-product, device/samsung/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
